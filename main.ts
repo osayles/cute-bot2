6af361e85a9a181cb6664a28ee3bce34559bd8f5
osayles <122713903+osayles@users.noreply.github.com>
2023-01-15 00:12:15 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,4 @@
 basic.showIcon(IconNames.Heart)
-music.startMelody(music.builtInMelody(Melodies.Funk), MelodyOptions.ForeverInBackground)
-cuteBot.colorLight(cuteBot.RGBLights.ALL, 0xff0000)
-cuteBot.colorLight(cuteBot.RGBLights.ALL, 0xff0000)
 cuteBot.forward()
 basic.pause(200)
 cuteBot.motors(100, 40)
